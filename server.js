7ff937ec3a56c11dd9871e0f562997935f8a5271
praveen2143 <praveensakthivel07@gmail.com>
2018-12-24 21:22:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static('./dist/nsetracker'));
 app.get('/*', function(req, res) {
   res.sendFile(path.join('./dist/nsetracker/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 443);
